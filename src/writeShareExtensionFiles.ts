01aaa8d68362920072a39db5219f12584438387a
TT <44315206+timedtext@users.noreply.github.com>
2023-04-11 16:30:08 +0800

    iOS: fix Share Extension freezes screen

--- src/writeShareExtensionFiles.ts
+++ src/writeShareExtensionFiles.ts
@@ -143,128 +143,112 @@ export function getShareExtensionViewControllerPath(
 export function getShareExtensionViewControllerContent(scheme: string) {
   console.debug("************ scheme", scheme);
 
-  return `import UIKit
-import Social
-import MobileCoreServices
-
-class ShareViewController: UIViewController {
+  return `import MobileCoreServices
+  import Social
+  import UIKit
+  
+  class ShareViewController: UIViewController {
     // IMPORTANT: This should be your host app scheme
     let hostAppURLScheme = "${scheme}"
     let urlContentType = kUTTypeURL as String
     let textContentType = kUTTypePlainText as String
-    
+  
     override func viewDidLoad() {
-        
-        var strUrl:String? = nil
-        DispatchQueue.global().async {
-            
-            
-            if let content = self.extensionContext!.inputItems[0] as? NSExtensionItem {
-                if let contents = content.attachments {
-                    for (_, attachment) in (contents).enumerated() {
-                        if attachment.hasItemConformingToTypeIdentifier(self.urlContentType) {
-                            let _strUrl = self.getStrUrlFromUrl(attachment: attachment)
-                            if(_strUrl != nil){
-                                strUrl = _strUrl
-                            }
-                        }
-                        
-                        if attachment.hasItemConformingToTypeIdentifier(self.textContentType) {
-                            if(strUrl != nil){
-                                continue;
-                            }
-                            let    _strUrl = self.getStrUrlFromText(attachment: attachment)
-                            if(_strUrl != nil){
-                                strUrl = _strUrl
-                            }
-                            
-                        }
-                    }
-                }
-            }
-            
-            if(strUrl == nil){
-                self.dismissWithError()
-                return;
-            }
-            self.redirectToHostApp(sharedURL: strUrl!)
-        }
+      super.viewDidLoad()
     }
-    
-    
-    private func getStrUrlFromUrl ( attachment: NSItemProvider) -> String? {
-        var result:String? = nil
-        
-        let semaphore = DispatchSemaphore(value: 0)
-        
-        attachment.loadItem(forTypeIdentifier: self.urlContentType, options: nil) { data, error in
-            
-            if error == nil, let item = data as? URL {
-                let _url = URL(string: item.absoluteString)
-                if(_url != nil){
-                    result = item.absoluteString
-                }
+  
+    override func viewDidAppear(_ animated: Bool) {
+      super.viewDidAppear(animated)
+  
+      if let content = self.extensionContext!.inputItems[0] as? NSExtensionItem {
+        if let contents = content.attachments {
+          for (_, attachment) in (contents).enumerated() {
+            if attachment.hasItemConformingToTypeIdentifier(self.urlContentType) {
+              self.handleUrl(attachment: attachment)
+            } else if attachment.hasItemConformingToTypeIdentifier(self.textContentType) {
+              self.handleText(attachment: attachment)
             }
-            semaphore.signal()
+          }
         }
-        
-        semaphore.wait()
-        
-        return result
+      }
     }
-    
-    private func getStrUrlFromText ( attachment: NSItemProvider) -> String? {
-        var result:String? = nil
-        
-        let semaphore = DispatchSemaphore(value: 0)
-        
-        attachment.loadItem(forTypeIdentifier: self.textContentType, options: nil) { data, error in
-            
-            if error == nil, let item = data as? String {
-                
-                let types: NSTextCheckingResult.CheckingType = [.link]
-                let detector = try? NSDataDetector(types: types.rawValue)
-                
-                if  detector != nil && item.count > 0 && detector!.numberOfMatches(in: item, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, item.count)) > 0 {
-                    result = item
-                }
-                
-            }
-            semaphore.signal()
+  
+    private func handleUrl(attachment: NSItemProvider) {
+  
+      attachment.loadItem(forTypeIdentifier: self.urlContentType, options: nil) { data, error in
+        var result: String? = nil
+  
+        if error == nil, let item = data as? URL {
+          let _url = URL(string: item.absoluteString)
+          if _url != nil {
+            result = item.absoluteString
+          }
         }
-        
-        semaphore.wait()
-        
-        
-        return result
-    }    
-    
+  
+        if result == nil {
+          self.dismissWithError()
+          return
+        }
+  
+        self.redirectToHostApp(sharedURL: result!)
+      }
+    }
+  
+    private func handleText(attachment: NSItemProvider) {
+  
+      attachment.loadItem(forTypeIdentifier: self.textContentType, options: nil) { data, error in
+        var result: String? = nil
+  
+        if error == nil, let item = data as? String {
+  
+          let types: NSTextCheckingResult.CheckingType = [.link]
+          let detector = try? NSDataDetector(types: types.rawValue)
+  
+          if detector != nil && item.count > 0
+            && detector!.numberOfMatches(
+              in: item, options: NSRegularExpression.MatchingOptions(rawValue: 0),
+              range: NSMakeRange(0, item.count)) > 0
+          {
+            result = item
+          }
+        }
+  
+        if result == nil {
+          self.dismissWithError()
+          return
+        }
+  
+        self.redirectToHostApp(sharedURL: result!)
+      }
+    }
+  
     private func dismissWithError() {
-        self.dismiss(animated: true, completion: nil)
-        extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+      self.dismiss(animated: true, completion: nil)
+      extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
     }
-    
+  
     private func redirectToHostApp(sharedURL: String) {
-        var urlComponents = URLComponents()
-        urlComponents.scheme = hostAppURLScheme
-        urlComponents.host = "share"
-        urlComponents.path = "/"
-        urlComponents.queryItems = [
-            URLQueryItem(name: "url", value: sharedURL),
-        ]
-        // urlComponents.url: \(scheme)://share/?url=\(sharedURL)
-        let url = urlComponents.url
-        var responder = self as UIResponder?
-        let selectorOpenURL = sel_registerName("openURL:")
-        
-        while (responder != nil) {
-            if (responder?.responds(to: selectorOpenURL))! {
-                responder?.perform(selectorOpenURL, with: url)
-            }
-            responder = responder!.next
+      var urlComponents = URLComponents()
+      urlComponents.scheme = hostAppURLScheme
+      urlComponents.host = "share"
+      urlComponents.path = "/"
+      urlComponents.queryItems = [
+        URLQueryItem(name: "url", value: sharedURL)
+      ]
+      // urlComponents.url: \(scheme)://share/?url=\(sharedURL)
+      let url = urlComponents.url
+      var responder = self as UIResponder?
+      let selectorOpenURL = sel_registerName("openURL:")
+  
+      while responder != nil {
+        if (responder?.responds(to: selectorOpenURL))! {
+          responder?.perform(selectorOpenURL, with: url)
         }
-        extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+        responder = responder!.next
+      }
+  
+      extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
     }
-}
+  }
 `;
 }
